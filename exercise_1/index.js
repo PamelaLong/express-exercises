6346db5e791cef9a6d1bf09d62b8cd40eca5e7cf
pamela long <usmc.pnlong@gmail.com>
2021-09-08 16:15:50 -0400

    exercise 1

--- exercise_1/index.js
+++ exercise_1/index.js
@@ -1,28 +1,27 @@
-const PORT = 3000
-const express = require("express")
-const app = express()
+const PORT = 5501
+const express = require("express");
+const app = express();
 
-app("/", (req,res)=>{
-    res(console.log("Why hello there!"))
+app.get("/", (req,res)=> {
+    res.send(console.log("Why hello there!"));
 })
 
-app.post("@", () => {
-    req.send("🥭 is 🔥")
+app.get("/", (req, res) => {
+    res.send(console.log("🥭 is 🔥"));
 })
 
-post.app(app, () => {
-    res.send("I wonder what's wrong with this route 🤔?")
+app.get("/", (req, res) => {
+    res.send(console.log("I wonder what's wrong with this route 🤔?"));
 })
 
-app.get( (req,res) => {
-    res.send("Happy 🐶 loves yummy dog food!")
+app.get("/", (req, res) => {
+    res.send(console.log("Happy 🐶 loves yummy dog food!"));
 })
 
-app("/fruitsmoothie",() => {
-    res.send()
-    res.send("A 🥭, 🍍, 🍓 make a wonderful smoothie!")
+app.post("/fruitsmoothe",(req, res) => {
+    res.send(console.log("A 🥭, 🍍, 🍓 make a wonderful smoothie!"));
 })
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
-})
+});
